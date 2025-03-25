7f6a20147e440f4fc8308a34ef70bb3c7746f124
vanyaK54 <vanyakaushik54@gmail.com>
2025-03-25 11:29:49 -0400

    add new test case

--- COVID19/tests/test.py
+++ COVID19/tests/test.py
@@ -38,6 +38,22 @@ class TestCovidCases(unittest.TestCase):
         self.assertNotIn('MS Zaandam', countries)
         self.assertNotIn('Diamond Princess', countries)
 
+    def test_consistency_across_case_types(self):
+        """
+        Test that confirmed cases equal the sum of active, recovered, and dead cases
+        for each country on each date.
+        """
+        for country in self.confirmed_cases.columns[1:]:  # Skip 'Date' column
+            # Sum of active, recovered, and dead cases
+            total_cases = (self.active_cases[country] + 
+                           self.recovered_cases[country] + 
+                           self.dead_cases[country])
+
+            # Assert equality with confirmed cases
+            pd.testing.assert_series_equal(self.confirmed_cases[country], total_cases,
+                                           check_dtype=False,
+                                           obj=f"Data consistency check failed for {country}")
+
 if __name__ == '__main__':
 
     unittest.main()
